bed8a6bf5940fc18e1ca5eea8e22980d8b2c40ab
kirito <kirito666codr@gmail.com>
2025-09-08 20:59:41 +0530

    Add squiggle animation to logos and integrate SVGFilters in layout for enhanced visual effects

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -7,6 +7,8 @@ import { PrismicRichText, PrismicText, SliceComponentProps } from "@prismicio/re
 import { Bounded } from "@/components/Bounded";
 import { Heading } from "@/components/Heading";
 import { ButtonLink } from "@/components/ButtonLink";
+import { WideLogo } from "./WideLogo";
+import { TallLogo } from "./TallLogo";
 
 
 /**
@@ -24,6 +26,11 @@ const Hero: FC<HeroProps> = ({ slice }) => {
       data-slice-variation={slice.variation}
       className="bg-brand-pink relative h-dvh overflow-hidden text-zinc-800 bg-texture"
     >
+      <div className="absolute inset-0 flex items-center pt-20">
+         <WideLogo className="w-full text-brand-purple hidden opacity-20 mix-blend-multiply lg:block"/>
+         <TallLogo  className="w-full text-brand-purple block opacity-20 mix-blend-multiply lg:hidden"/>
+      </div>
+
       <div className="grid absolute inset-0 mx-auto mt-24 max-w-6xl grid-rows-[1fr_auto] place-items-start px-6 py-10 md:py-16">
       <Heading className="text-5xl font-extrabold relative  max-w-sm self-start">
      <PrismicText field={slice.primary.heading} />
