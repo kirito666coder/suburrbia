e9da921dd97e50ac2778887a68e1184feede56fc
kirito <kirito666codr@gmail.com>
2025-09-08 22:01:10 +0530

    Enhance Prismic types by adding Settings and Skateboard document definitions, and integrate ProductGrid slice into the project. Update Header component to fetch navigation settings dynamically from Prismic.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,18 @@
 import Link from "next/link"
 import { ButtonLink } from "./ButtonLink"
 import { Logo } from "./Logo"
+import { createClient } from "@/prismicio"
+import { PrismicNextLink } from "@prismicio/next"
 
 
 
-export const Header = () => {
+export const Header = async() => {
+
+    const client = createClient()
+
+    const settings = await client.getSingle('settings')
+
+
   return (
     <header className="absolute top-0 left-0 right-0 z-50 h-32 md:48 px-4 md:px-6 py-4 md:py-6 hd:h-32">
         <div className="mx-auto grid w-full max-w-6xl grid-cols-[1fr_auto_auto] items-center gap-6 md:grid-cols-[1fr_auto_1fr] ">
@@ -13,7 +21,11 @@ export const Header = () => {
             </Link>
             <nav aria-label="Main" className="col-span-full row-start-2  md:col-span-1 md:col-start-2 md:row-start-1">
                 <ul className="flex flex-wrap items-center justify-center gap-8">
-                    <li>Boards</li>
+                    {settings.data.navigation.map((item)=>(
+                    <li key={item.link.text}>
+                        <PrismicNextLink field={item.link} className="text-lg md:text-xl"/>
+                    </li>
+                    ))}
                 </ul>
             </nav>
             <div className="justify-self-end">
