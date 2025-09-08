9c37b91075c4c33c5bf39aaaffe2b288e51e7603
kirito <kirito666codr@gmail.com>
2025-09-08 23:37:39 +0530

    Add vertical and horizontal lines to SkateboardProduct component for improved visual separation and styling consistency.

--- src/slices/ProductGrid/SkateboardProduct.tsx
+++ src/slices/ProductGrid/SkateboardProduct.tsx
@@ -1,11 +1,18 @@
 import { ButtonLink } from '@/components/ButtonLink'
+import { HorizontalLine, VerticalLine } from '@/components/Line'
 import { createClient } from '@/prismicio'
 import { Content, isFilled } from '@prismicio/client'
 import { PrismicNextImage } from '@prismicio/next'
+import clsx from 'clsx'
 import { FaStar } from 'react-icons/fa6'
 type Props = {
     id:string,
 }
+
+const VERTICAL_LINK_CLASSES = 'absolute top-0 h-full stroke-2 text-stone-300 transition-colors group-hover:text-stone-400'
+
+const HORIZONTAL_LINK_CLASSES = ' -mx-8 stroke-2 text-stone-300 transition-colors group-hover:text-stone-400'
+
 
 const SkateboardProduct = async({id}: Props) => {
     const client = createClient()
@@ -18,6 +25,13 @@ const SkateboardProduct = async({id}: Props) => {
 
   return (
     <div className='group relative mx-auto w-full max-w-72 px-8 pt-4 '>
+
+      <VerticalLine className={clsx(VERTICAL_LINK_CLASSES, 'left-4')}/>
+      <VerticalLine className={clsx(VERTICAL_LINK_CLASSES, 'right-4')}/>
+      <HorizontalLine className={HORIZONTAL_LINK_CLASSES}/>
+
+
+
         <div className='flex items-center justify-between text-sm md:text-2xl'>
             <span>{price}</span>
             <span className='inline-flex items-center gap-1'>
@@ -27,6 +41,7 @@ const SkateboardProduct = async({id}: Props) => {
         <div className='-mb-1 overflow-hidden py-4'>
         <PrismicNextImage alt="" field={product.data.image} width={150} className='mx-auto w-[58%] origin-top transform-gpu transition-transform duration-500 ease-in-out group-hover:scale-150'/>
         </div>
+        <HorizontalLine className={HORIZONTAL_LINK_CLASSES}/>
 
         <h3 className='my-2 text-center font-sans leading-tight text-lg md:text-xl font-extrabold'>
             {product.data.name}
