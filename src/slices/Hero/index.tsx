271f3bce8cdb2c127fcf1bcbe9165d82afcec003
kirito <kirito666codr@gmail.com>
2025-09-08 15:22:53 +0530

    Enhance styling and layout by adding new brand colors, updating font imports, and refactoring the Hero component to use a Bounded wrapper and improved structure.

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,7 +1,13 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, PrismicText, SliceComponentProps } from "@prismicio/react";
 import { PrismicNextLink } from "@prismicio/next";
+
+
+
+import { Bounded } from "@/components/Bounded";
+import { Heading } from "@/components/Heading";
+
 
 /**
  * Props for `Hero`.
@@ -13,14 +19,23 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero: FC<HeroProps> = ({ slice }) => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="bg-brand-pink relative h-dvh overflow-hidden text-zinc-800 bg-texture"
     >
-     <PrismicRichText field={slice.primary.heading} />
+      <div className="grid absolute inset-0 mx-auto mt-24 max-w-6xl grid-rows-[1fr.auto] place-items-start px-6 ~py-10/16">
+      <Heading className="text-5xl font-extrabold relative  max-w-sm self-start">
+     <PrismicText field={slice.primary.heading} />
+      </Heading>
+      <div className="flex justify-between gap-30">
      <PrismicRichText field={slice.primary.body} />
+     <div className=" text-nowrap">
      <PrismicNextLink field={slice.primary.button} />
-    </section>
+     </div>
+      </div>
+      </div>
+    </Bounded>
   );
 };
 
