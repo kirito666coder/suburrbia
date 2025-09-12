307827b766123b58d00ac0bcbadaa50bee2537ed
kirito <kirito666codr@gmail.com>
2025-09-12 15:56:55 +0530

    Integrate FooterPhysics component into Footer, fetching skateboard texture URLs from settings for enhanced visual dynamics.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,22 +3,23 @@ import { PrismicNextImage, PrismicNextLink } from '@prismicio/next'
 import React from 'react'
 import { Logo } from './Logo'
 import { Bounded } from './Bounded'
+import { FooterPhysics } from './FooterPhysics'
+import { asImageSrc } from '@prismicio/client'
 
-type Props = {}
-
-export const Footer = async ({}: Props) => {
+export const Footer = async () => {
 
     const client = createClient()
 
     const settings = await client.getSingle('settings')
 
+    const boardTextureURLs = settings.data.footer_skateboards.map((item)=> asImageSrc(item.skateboard,{h:600})).filter((url):url is string => Boolean(url))
 
   return (
     <footer className='bg-texture bg-zinc-900 text-white overflow-hidden'>
       
      <div className='relative h-[75vh] p-10 md:p-16 md:aspect-auto'>
         <PrismicNextImage field={settings.data.footer_image} alt='' fill className='object-cover ' width={1200}/>
-
+        <FooterPhysics boardTextureURLs={boardTextureURLs} className='absolute inset-0 overflow-hidden' />
         <Logo className='pointer-events-none relative h-20 mix-blend-exclusion md:h-28'/>
 
      </div>
