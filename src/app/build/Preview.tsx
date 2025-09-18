8d61f514cc24d9bbf93922d5de760f233d76d54f
kirito <kirito666codr@gmail.com>
2025-09-18 23:40:16 +0530

    Implement board customizer functionality in page.tsx, integrating CustomizerControlsProvider and Preview components. Fetch and set default settings for wheels, decks, and metals from Prismic, enhancing user interaction and experience.

---
+++ src/app/build/Preview.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+type Props = {
+    wheelTextureURLs:string[];
+    deckTextureURLs:string[];
+
+}
+
+export default function Preview({wheelTextureURLs,deckTextureURLs}: Props) {
+  return (
+    <div>
+        
+    </div>
+  )
+}
--- src/app/build/page.tsx
+++ src/app/build/page.tsx
@@ -5,12 +5,43 @@ import { Logo } from '@/components/Logo'
 import Link from 'next/link'
 import React from 'react'
 
-type Props = {}
+import { CustomizerControlsProvider } from './context'
+import { createClient } from '@/prismicio'
+import Preview from './Preview'
+import { asImageSrc } from '@prismicio/client'
 
-export default async function page ({}: Props) {
+
+export default async function page () {
+
+    const client = createClient();
+    const customizerSettings = await client.getSingle("board_costomizer")
+    const {wheels,decks,metals} = customizerSettings.data;
+
+
+    const defaultWheel = wheels[0];
+    const defaultDeck = decks[0];
+    const defaultTruck = metals[0];
+    const defaultBolt = metals[0];
+
+
+    const wheelTextureURLs = wheels.map((texutre) => asImageSrc(texutre.texture)).filter((url):url is string => Boolean(url));
+    const deckTextureURLs = decks.map((texutre) => asImageSrc(texutre.texture)).filter((url):url is string => Boolean(url));
+
   return (
     <div className='flex h-screen flex-col lg:flex-row'>
+        <CustomizerControlsProvider
+        defaultWheel={defaultWheel}
+        defaultDeck={defaultDeck}
+        defaultTruck={defaultTruck}
+        defaultBolt={defaultBolt}
+        >
         <div className="relative aspect-square shrink-0 bg-[#3a414a] lg:aspect-auto lg:grow">
+       
+       <div className='absolute inset-0'>
+        <Preview wheelTextureURLs={wheelTextureURLs} deckTextureURLs={deckTextureURLs} />
+       </div>
+
+
        <Link href={'/'} className='absolute left-6 top-6' />
        <Logo className='h-12 text-white'/>
         </div>
@@ -19,11 +50,12 @@ export default async function page ({}: Props) {
         Build your board
         </Heading>
 
-        
+
         <ButtonLink href={''} color='lime' icon='plus' >
             Add to cart
         </ButtonLink>
         </div>
+        </CustomizerControlsProvider>
     </div>
   )
 }
